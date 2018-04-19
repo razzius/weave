b7999f0af57e079f21256edf43e741f7758de384
Razzi Abuissa <razzi53@gmail.com>
2018-04-19 09:48:40 -0700

    Fun with Meta metaclass

--- server.py
+++ server.py
@@ -1,9 +1,17 @@
-from flask import Flask, url_for, request, redirect, jsonify
+from datetime import datetime
+
+from flask import Flask, jsonify
+
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+from sqlalchemy import PrimaryKeyConstraint
 
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+ma = Marshmallow(app)
 
 
 class Profile(db.Model):
@@ -23,44 +31,37 @@ class ProfileInterest(db.Model):
 
     clinical = db.Column(db.Boolean)
 
-    profile_id = db.Column(db.Integer, db.ForeignKey('category.id'), nullable=False)
+    profile_id = db.Column(db.Integer, db.ForeignKey(Profile.id), nullable=False)
     profile = db.relationship('Profile', backref=db.backref('posts', lazy=True))
+
+    __table_args__ = (
+        PrimaryKeyConstraint('interest', 'clinical', 'profile_id', name='interest_pk'),
+    )
+
+
+class SchemaFieldsMeta(ma.Schema.__class__):
+    def __new__(cls, name, bases, attrs):
+        if 'Meta' in attrs:
+            attrs['Meta'].fields = [column.name for column in attrs['Meta'].model.__table__.columns]
+        return super().__new__(cls, name, bases, attrs)
+
+
+class MagicSchema(ma.Schema, metaclass=SchemaFieldsMeta):
+    pass
+
+
+class ProfileSchema(MagicSchema):
+    class Meta:
+        model = Profile
+
+
+profile_schema = ProfileSchema()
+profiles_schema = ProfileSchema(many=True)
 
 
 @app.route('/api/profiles')
 def get_profiles():
-    return jsonify([
-        {
-            'id': 1,
-            'name': 'Razzi Abuissa',
-            'email': 'rabuissa@sas.upenn.edu',
-            'affiliations': ['PennMed'],
-            'interests': ['yoga', 'software'],
-            'clinicalInterests': [],
-            'levelOfInvolvement': ['low'],
-            'meetingFrequency': ['monthly']
-        },
-        {
-            'id': 2,
-            'name': 'Jon Kusner',
-            'email': 'jon.kusner8@harvard.edu',
-            'affiliations': ['Boston Hospital'],
-            'clinicalInterests': ['Cardiology'],
-            'interests': ['archery', 'pottery'],
-            'levelOfInvolvement': ['high'],
-            'meetingFrequency': ['weekly']
-        },
-        {
-            'id': 3,
-            'name': 'Jane',
-            'email': 'jane.swimmer@harvard.edu',
-            'affiliations': ['Mercy Hospital'],
-            'interests': ['mentorship', 'swimming'],
-            'clinicalInterests': [],
-            'levelOfInvolvement': ['high'],
-            'meetingFrequency': ['weekly']
-        }
-    ])
+    return profiles_schema.jsonify(Profile.query.all())
 
 
 @app.route('/api/profile/<profile_id>')
@@ -74,13 +75,14 @@ def create_profile(profile_id=None):
     db.session.add(profile)
     db.session.commit()
 
-    return jsonify(serialize_profile(profile))
+    profile_schema = ProfileSchema()
+    return profile_schema.dump(profile).data
 
 
-@app.route('/api/profile/<profile_id>', methods=['POST'])
+@app.route('/api/profile/<profile_id>', methods=['PUT'])
 def update_profile(profile_id=None):
     profile = Profile({})
     db.session.add(profile)
     db.session.commit()
 
-    return jsonify(serialize_profile(profile))
+    return jsonify(ProfileSchema().dump(profile).data)
