d5f4bb83e5019a181ebc570cadccca688b07c255
Razzi Abuissa <razzi53@gmail.com>
2018-04-23 20:21:00 -0700

    Might just go to string interests etc

--- server.py
+++ server.py
@@ -1,22 +1,21 @@
+import json
 from datetime import datetime
 
-from flask import Flask, jsonify, request, send_from_directory, Response
+from flask import Flask, Response, jsonify, request, send_from_directory
 
+from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
-from marshmallow import fields
-from marshmallow_sqlalchemy import ModelSchema
+from marshmallow import Schema, ValidationError, fields, pre_load
 from sqlalchemy import PrimaryKeyConstraint
 from sqlalchemy.ext.declarative import declared_attr
+from sqlalchemy.sql import exists
 
 
-app = Flask(
-    __name__,
-    static_url_path='/static',
-    static_folder='build/static'
-)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app = Flask(__name__, static_url_path="/static", static_folder="build/static")
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:////tmp/test.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
+CORS(app)
 
 
 class Profile(db.Model):
@@ -24,86 +23,124 @@ class Profile(db.Model):
     name = db.Column(db.String(255), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
 
+    personal_interests = db.Column(db.String(1000))
+
     date_created = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     date_updated = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
 
     def __repr__(self):
-        return f'<Profile id={self.id} name={self.name}>'
+        return f"<Profile id={self.id} name={self.name}>"
 
 
-class Interest():
-    interest = db.Column(db.String(120))
+# class Interest():
+#     interest = db.Column(db.String(120))
 
-    @declared_attr
-    def profile_id(cls):
-        return db.Column(db.Integer, db.ForeignKey(Profile.id), nullable=False)
+#     @declared_attr
+#     def profile_id(cls):
+#         return db.Column(db.Integer, db.ForeignKey(Profile.id), nullable=False)
 
-    @declared_attr
-    def __table_args__(cls):
-        return (
-            PrimaryKeyConstraint('interest', 'profile_id', name='interest_pk'),
-        )
+#     @declared_attr
+#     def __table_args__(cls):
+#         return (PrimaryKeyConstraint("interest", "profile_id", name="interest_pk"),)
 
 
-class PersonalInterest(db.Model, Interest):
-    profile = db.relationship('Profile', backref=db.backref('personal_interests', lazy=True))
+# class PersonalInterest(db.Model, Interest):
+#     profile = db.relationship(
+#         "Profile", backref=db.backref("personal_interests", lazy=True)
+#     )
 
 
-class ClinicalInterest(db.Model, Interest):
-    profile = db.relationship('Profile', backref=db.backref('clinical_interests', lazy=True))
+# class ClinicalInterest(db.Model, Interest):
+#     profile = db.relationship(
+#         "Profile", backref=db.backref("clinical_interests", lazy=True)
+#     )
 
 
-class PersonalInterestSchema(ModelSchema):
+# class Affiliation(db.Model):
+#     affiliation = db.Column(db.String(120))
+
+#     profile_id = db.Column(db.Integer, db.ForeignKey(Profile.id), nullable=False)
+#     profile = db.relationship("Profile", backref=db.backref("affiliations", lazy='dynamic'))
+
+#     @declared_attr
+#     def __table_args__(cls):
+#         return (PrimaryKeyConstraint("affiliation", "profile_id", name="interest_pk"),)
+
+
+class PersonalInterestSchema(Schema):
+
     class Meta:
         model = PersonalInterest
 
 
-class ClinicalInterestSchema(ModelSchema):
+class ClinicalInterestSchema(Schema):
+
     class Meta:
         model = ClinicalInterest
 
 
-class ProfileSchema(ModelSchema):
-    personal_interests = fields.Nested(PersonalInterestSchema, many=True, exclude=('profile',))
-    clinical_interests = fields.Nested(ClinicalInterestSchema, many=True, exclude=('profile',))
+class AffiliationSchema(Schema):
 
     class Meta:
-        model = Profile
-        fields = ['id', 'name', 'email', 'personal_interests', 'clinical_interests']
+        model = Affiliation
+
+
+class ProfileSchema(Schema):
+    id = fields.Int(dump_only=True)
+    name = fields.String()
+    email = fields.String()
+
+    # personal_interests = fields.Nested(
+    #     PersonalInterestSchema, many=True, exclude=("profile",)
+    # )
+    # clinical_interests = fields.Nested(
+    #     ClinicalInterestSchema, many=True, exclude=("profile",)
+    # )
+    # affiliations = fields.Nested(AffiliationSchema, many=True, exclude=("profile",))
+
+    @pre_load
+    def process_personal_interests(self, data):
+        data['personal_interests'] = [
+            {'interest': interest} for interest in data['personal_interests']
+        ]
+        return data
 
 
 profile_schema = ProfileSchema()
 profiles_schema = ProfileSchema(many=True)
 
 
-@app.route('/')
-@app.route('/<path:path>')
+@app.route("/")
+@app.route("/<path:path>")
 def index(path=None):
-    return send_from_directory('build', 'index.html')
+    return send_from_directory("build", "index.html")
 
 
-@app.route('/api/profiles')
+@app.route("/api/profiles")
 def get_profiles():
-    return jsonify(profiles_schema.dump(Profile.query.all()).data)
+    return jsonify(profiles_schema.dump(Profile.query.all()))
 
 
-@app.route('/api/profile/<profile_id>')
+@app.route("/api/profile/<profile_id>")
 def get_profile(profile_id=None):
     return {}
 
 
-@app.route('/api/profile', methods=['POST'])
+def error(reason):
+    return Response(json.dumps(reason), status=400, content_type="application/json")
+
+
+@app.route("/api/profile", methods=["POST"])
 def create_profile(profile_id=None):
-    data = request.json
-    errors = profile_schema.validate(data, session=db.session)
+    json_data = request.get_json()
 
-    if errors:
-        return Response(errors, status=400, content_type='application/json')
+    try:
+        data = profile_schema.load(json_data)
+    except ValidationError as err:
+        return jsonify(err.messages), 422
 
-    if Profile.query.filter(Profile.email == data['email']).count > 0:
-        return Response({'email': ['This email already exists in the database']},
-                        status=400,
-                        content_type='application/json')
+    if db.session.query(exists().where(Profile.email == data["email"])).scalar():
+        return error({"email": ["This email already exists in the database"]})
 
     profile = Profile(**data)
     db.session.add(profile)
@@ -112,7 +149,7 @@ def create_profile(profile_id=None):
     return profile_schema.dump(profile).data
 
 
-@app.route('/api/profile/<profile_id>', methods=['PUT'])
+@app.route("/api/profile/<profile_id>", methods=["PUT"])
 def update_profile(profile_id=None):
     profile = Profile({})
     db.session.add(profile)
