a0cd756953baba1e73c259facbc80e85c44159ae
Razzi Abuissa <razzi53@gmail.com>
2018-05-29 23:12:09 -0700

    Additional resource links on home page

--- src/App.js
+++ src/App.js
@@ -12,38 +12,39 @@ import Profile from "./Profile"
 import "slick-carousel/slick/slick.css"
 import "slick-carousel/slick/slick-theme.css"
 
-export default class App extends Component {
-  render() {
-    return (
-      <Router>
-        <div className="App">
-          <header className="App-header">
-            <div className="header-inner">
-              <Link to="/" className="App-title">
-                <h1>HMS Advises</h1>
-              </Link>
-            </div>
-          </header>
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route
-              path="/student-expectations"
-              component={StudentExpectations}
-            />
-            <Route
-              path="/faculty-expectations"
-              component={FacultyExpectations}
-            />
-            <Route path="/edit-profile" component={EditProfile} />
-            <Route path="/browse" component={Browse} />
-            <Route
-              path="/profiles/:id"
-              render={props => <Profile {...props} />}
-            />
-            <Route component={() => <p>404 Not found</p>} />
-          </Switch>
+const App = () => (
+  <Router>
+    <div className="App">
+      <header className="App-header">
+        <div className="header-inner">
+          <Link to="/" className="App-title">
+            <h1>HMS Advises</h1>
+          </Link>
+          <a href="/#about" className="App-title">
+            About
+          </a>
         </div>
-      </Router>
-    )
-  }
-}
+      </header>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route
+          path="/student-expectations"
+          component={StudentExpectations}
+          />
+        <Route
+          path="/faculty-expectations"
+          component={FacultyExpectations}
+          />
+        <Route path="/edit-profile" component={EditProfile} />
+        <Route path="/browse" component={Browse} />
+        <Route
+          path="/profiles/:id"
+          render={props => <Profile {...props} />}
+          />
+          <Route component={() => <p>404 Not found</p>} />
+      </Switch>
+    </div>
+  </Router>
+)
+
+export default App
--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,7 @@
 import React from "react"
 import { Link } from "react-router-dom"
 
-export default () => (
+const Home = () => (
   <div className="Home">
     <main id="background">
       <div className="App-intro">
@@ -15,9 +15,29 @@ export default () => (
         </Link>
       </div>
     </main>
-    <section className="about">
+    <section id="about">
       <h1>About</h1>
-      This mentorship program and platform was developed to provide a customized way for students to find invested mentors for professional and/or personal interests. While resources for faculty contact information exist, there are often unclear guidelines about what a relationship will constitute after a student reaches out and varying degrees of involvement with which faculty may feel comfortable engaging in a mentorship role. This program provides an opt-in mentoring structure with clarified expectations for both mentors and mentees that may encourage more longitudinal and hopefully meaningful mentoring experiences. Through personalizing their profiles, mentors may share a preferred cadence for meeting with students, and may indicate in what areas and in what capacities they would like to mentor students. Furthermore, the “clinical interests” and “additional interests” tags and search feature provide opportunities for students to find mentors with whom they may discuss career interests, the process of discovering and developing these interests, and integrating additional aspects such as balancing family life, passion for social justice advocacy, and more.
+      <p>This mentorship program and platform was developed to provide a customized way for students to find invested mentors for professional and/or personal interests. While resources for faculty contact information exist, there are often unclear guidelines about what a relationship will constitute after a student reaches out and varying degrees of involvement with which faculty may feel comfortable engaging in a mentorship role. This program provides an opt-in mentoring structure with clarified expectations for both mentors and mentees that may encourage more longitudinal and hopefully meaningful mentoring experiences. Through personalizing their profiles, mentors may share a preferred cadence for meeting with students, and may indicate in what areas and in what capacities they would like to mentor students. Furthermore, the “clinical interests” and “additional interests” tags and search feature provide opportunities for students to find mentors with whom they may discuss career interests, the process of discovering and developing these interests, and integrating additional aspects such as balancing family life, passion for social justice advocacy, and more.</p>
+    </section>
+    <section id="additional-resources">
+      <h3>Additional Resources</h3>
+      <ul>
+        <li>
+          <a href="https://medstudenthandbook.hms.harvard.edu/1019-thrive-mobile-app">Thrive Mobile App</a>
+        </li>
+        <li>
+          <a href="https://meded.hms.harvard.edu/student-advising">Student Advising</a>
+        </li>
+        <li>
+          <a href="https://collaborate.hms.harvard.edu/display/hmsadvising/Specialty+Advising">Specialty Advising</a>
+        </li>
+        <li>
+          <a href="https://collaborate.hms.harvard.edu/display/hmsadvising/Personal+Advising">Personal Advising</a>
+        </li>
+      </ul>
     </section>
   </div>
 )
+
+export default Home
+
