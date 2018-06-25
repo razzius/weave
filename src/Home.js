606797302273262224e1ed5ecd4b36096da92b00
Razzi Abuissa <razzi53@gmail.com>
2018-06-25 14:35:11 -0700

    Update "about" verbiage

--- src/Home.js
+++ src/Home.js
@@ -18,7 +18,8 @@ const Home = () => (
     <section id="about">
       <h1>About</h1>
       <p>
-        Soon after entering medical school, students are faced with a new learning and social environment and pressure to quickly determine academic or clinical interests. Although resources for faculty contact information exist, both students and faculty have found there to be unclear expectations about what a mentorship relationship or experience will constitute. Furthermore, for students with myriad interests who are still in the process of discovering their desired field of medicine, it is often challenging to know who to reach out to when lists of faculty members are provided solely based on clinical specialty.      </p>
+        Soon after entering medical school, students are faced with a new learning and social environment and pressure to quickly determine academic or clinical interests. Although resources for faculty contact information exist, both students and faculty have found there to be unclear expectations about what a mentorship relationship or experience will constitute. Furthermore, for students with myriad interests who are still in the process of discovering their desired field of medicine, it is often challenging to know who to reach out to when lists of faculty members are provided solely based on clinical specialty.
+      </p>
 
       <p>
         We developed a mentorship platform that provides an interest-driven way for students to find invested mentors for professional or personal guidance. In recognizing that students can only set expectations for their side of the mentoring relationship, our platform will allow faculty to more clearly communicate their own level of comfort in engaging in a mentoring relationship. In recognizing that many clinical, academic, and personal demands exist for faculty, we hope to empower faculty to articulate for themselves what their level of mentorship involvement will be.
