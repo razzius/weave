08fe7f8eab37c30c3c9b998cf05c4e56d76e16c8
Razzi Abuissa <razzi53@gmail.com>
2018-07-05 16:21:44 -0700

    Update about

--- src/Home.js
+++ src/Home.js
@@ -18,15 +18,18 @@ const Home = () => (
     <section id="about">
       <h1>About</h1>
       <p>
-        This mentorship platform was developed to provide an interest-driven way for students to find invested mentors for professional or personal guidance. Although resources for faculty contact information exist, student have found there to be unclear expectations about what a relationship or experience will constitute from the faculty point of view. In recognizing that students can only set expectations for their side of the mentoring relationship, we designed a platform that allows faculty to more clearly communicate their own level of comfort in engaging in a mentoring relationship. In light of the many clinical, academic, and personal demands that exist for faculty, we hope to empower faculty to articulate for themselves their level of mentorship involvement.
+        Soon after entering medical school, students are faced with a new learning and social environment and pressure to quickly determine academic or clinical interests. Although resources for faculty contact information exist, both students and faculty have found there to be unclear expectations about what a mentorship relationship or experience will constitute. Furthermore, for students with myriad interests who are still in the process of discovering their desired field of medicine, it is often challenging to know who to reach out to when lists of faculty members are provided solely based on clinical specialty.      </p>
+
+      <p>
+        We developed a mentorship platform that provides an interest-driven way for students to find invested mentors for professional or personal guidance. In recognizing that students can only set expectations for their side of the mentoring relationship, our platform will allow faculty to more clearly communicate their own level of comfort in engaging in a mentoring relationship. In recognizing that many clinical, academic, and personal demands exist for faculty, we hope to empower faculty to articulate for themselves what their level of mentorship involvement will be.
       </p>
 
       <p>
-        This mentorship platform provides an opt-in, tailored mentoring structure with clear, adjustable expectations for both mentors and mentees in the hopes of encouraging more longitudinal and meaningful mentoring experiences.
+        On this platform, faculty may personalize their profiles, including sharing a preferred cadence for meeting with students, indicating topics that they are willing to discuss, and showing or hiding their profiles based on their availability. Students may search for mentors using both clinical and non-clinical “tags,” as well as view available faculty profiles. In each profile, students will see additional opportunities that each faculty offers and other preferences that faculty members have chosen for themselves.
       </p>
 
       <p>
-        For faculty, profiles may be personalized; faculty can share a preferred cadence for meeting with students, may indicate topics that they are willing to discuss, and can show or hide their profiles based on their availability. Students may search for mentors using both clinical and non-clinical “tags” as well as view available faculty profiles. In each profile, students will see additional opportunities that each faculty offers and other preferences that faculty members have chosen for themselves.
+        We hope that this mentorship platform provides an opt-in, customized mentoring structure with clear, adjustable expectations for both mentors and mentees that may encourage more longitudinal and meaningful mentoring experiences.
       </p>
     </section>
     <section id="additional-resources">
