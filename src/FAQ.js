0715d8138b6798a8ae84662db5e4d67b067ad896
Razzi Abuissa <razzi53@gmail.com>
2018-09-18 00:27:00 -0700

    Update FAQ

--- src/FAQ.js
+++ src/FAQ.js
@@ -4,8 +4,11 @@ import AppScreen from './AppScreen'
 const Resources = () => (
   <AppScreen className="resources">
     <iframe
-    frameBorder="0"
-width="100%" height="3400px" src="https://docs.google.com/document/d/e/2PACX-1vROI4dH-yO_6VtkPkCdmOrFOb9PBAqjs7ifJBxoGymt1ifEb3K-7hllXA-hr97AVv7h4LP8Ho4WrCMu/pub?embedded=true"></iframe>
+      frameBorder="0"
+      width="100%"
+      height="3400px"
+      src="https://docs.google.com/document/d/e/2PACX-1vROI4dH-yO_6VtkPkCdmOrFOb9PBAqjs7ifJBxoGymt1ifEb3K-7hllXA-hr97AVv7h4LP8Ho4WrCMu/pub?embedded=true"
+    />
   </AppScreen>
 )
 
