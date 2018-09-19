3830ad22c937fc3154acfcf9f9aba65ff5a98c0b
Razzi Abuissa <razzi53@gmail.com>
2018-09-19 09:44:11 -0700

    Make iframe taller

--- src/FAQ.js
+++ src/FAQ.js
@@ -6,7 +6,7 @@ const Resources = () => (
     <iframe
       frameBorder="0"
       width="100%"
-      height="3400px"
+      height="4000px"
       src="https://docs.google.com/document/d/e/2PACX-1vROI4dH-yO_6VtkPkCdmOrFOb9PBAqjs7ifJBxoGymt1ifEb3K-7hllXA-hr97AVv7h4LP8Ho4WrCMu/pub?embedded=true"
     />
   </AppScreen>
