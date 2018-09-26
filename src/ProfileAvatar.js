7f34df281385ac5269f3f07a15dabe795fd6d03e
Razzi Abuissa <razzi53@gmail.com>
2018-09-25 20:17:40 -0700

    Better avatar sizing?

--- src/ProfileAvatar.js
+++ src/ProfileAvatar.js
@@ -1,11 +1,18 @@
 import React from 'react'
 import Avatar from 'react-avatar'
 
-const ProfileAvatar = ({profileImageUrl, name}) =>
-  profileImageUrl ? (
-    <img alt="Profile" className="profile-image column" src={profileImageUrl} />
-  ) : (
-    <Avatar name={name} size={200} round textSizeRatio={3} />
-  )
+const ProfileAvatar = ({ profileImageUrl, name }) => (
+  <div className="avatar">
+    {profileImageUrl ? (
+      <img
+        alt="Profile"
+        className="profile-image column"
+        src={profileImageUrl}
+      />
+    ) : (
+      <Avatar name={name} size={200} round textSizeRatio={3} />
+    )}
+  </div>
+)
 
 export default ProfileAvatar
