dc10fc996230ddd84bcdd829d1e6510ddec02867
Razzi Abuissa <razzi.abuissa@goodeggs.com>
2018-10-06 14:33:23 -0700

    Switch to new-style sentry

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import 'url-polyfill'
+import { init } from '@sentry/browser'
 
 import './index.css'
 import App from './App'
+
+if (window.location.hostname !== 'localhost') {
+  init({
+    dsn: process.env.SENTRY_DSN
+  })
+}
 
 ReactDOM.render(<App />, document.getElementById('root'))
