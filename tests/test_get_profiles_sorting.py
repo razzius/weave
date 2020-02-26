1b2a9512205b121ee43c6b3e1c458c8e71e71ff7
Razzi Abuissa <razzi53@gmail.com>
2020-02-26 01:20:41 -0800

    Move some files around; and tdd failing tests test_get_public_tags.py

--- tests/test_get_profiles_sorting.py
+++ tests/test_get_profiles_sorting.py
@@ -1,46 +1,24 @@
-import uuid
 import datetime
-from server.models import Profile
 import http
 
-from server.models import VerificationEmail, VerificationToken, save
-
-
-def create_profile(email, name, date_updated):
-    verification_email = VerificationEmail(email=email)
-    save(verification_email)
-
-    verification_token = VerificationToken(token=str(uuid.uuid4()), email_id=verification_email.id)
-    save(verification_token)
-
-    profile = Profile(
-        name=name,
-        contact_email=email,
-        verification_email_id=verification_email.id,
-        cadence='monthly',
-        date_updated=date_updated
-    )
-    save(profile)
-    return profile
+from .utils import create_test_profile
 
 
 def test_sort_profiles_by_date_updated(client):
-    own_profile = create_profile(
+    own_profile = create_test_profile(
         email='test@test.com',
         name='Own Profile',
-        date_updated=datetime.datetime(2018, 1, 1)
+        date_updated=datetime.datetime(2018, 1, 1),
     )
 
-    recently_updated_profile = create_profile(
-        email='updated@test.com',
-        name='Z',
-        date_updated=datetime.datetime(2019, 10, 1)
+    recently_updated_profile = create_test_profile(
+        email='updated@test.com', name='Z', date_updated=datetime.datetime(2019, 10, 1)
     )
 
-    not_recently_updated_profile = create_profile(
+    not_recently_updated_profile = create_test_profile(
         email='not_updated@test.com',
         name='A',
-        date_updated=datetime.datetime(2017, 10, 1)
+        date_updated=datetime.datetime(2017, 10, 1),
     )
 
     own_token = own_profile.verification_email.verification_tokens[0].token
