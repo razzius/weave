903f4e716bd4902a6f6f47c4663c46801b5019ce
Razzi Abuissa <razzi53@gmail.com>
2020-03-26 20:16:53 -0700

    Add COVID19 banner. Closes #180

--- src/Home.js
+++ src/Home.js
@@ -1,6 +1,7 @@
 // @flow
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { LiteralLink } from './utils'
 
 import { type Account } from './api'
 
@@ -51,6 +52,16 @@ const Home = ({
 
   return (
     <div className="Home">
+      <p className="error banner" style={{ textAlign: 'center' }}>
+        During the current COVID-19 pandemic, we are encouraging mentor and
+        mentee pairs to meet over Zoom. All Harvard students and faculty receive
+        free Zoom access available at:{' '}
+        <LiteralLink href="https://harvard.zoom.us/" />. This recommendation
+        will remain active throughout the COVID-19 pandemic. More information
+        can be found on the CDC&apos;s website:
+        <br />
+        <LiteralLink href="https://www.cdc.gov/coronavirus/2019-ncov/index.html" />
+      </p>
       <div id="background">
         <div className="App-intro">
           {(token === null || isMentor === false) && (
