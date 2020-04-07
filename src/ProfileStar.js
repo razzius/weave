0706e05071c7dca5fbb0a4e5a833ca488c40c3fb
Razzi Abuissa <razzi53@gmail.com>
2020-04-07 10:43:47 -0700

    Enable pressing enter or space to star profile

--- src/ProfileStar.js
+++ src/ProfileStar.js
@@ -1,11 +1,24 @@
 // @flow
 import React from 'react'
 
-export default ({ onClick, active }: { onClick: Function, active: boolean }) => (
+export default ({
+  onClick,
+  active,
+}: {
+  onClick: ?Function,
+  active: boolean,
+}) => (
   <img
+    tabIndex="1"
     alt="Star"
     className={`profile-star ${active ? 'active' : ''}`}
     src="/assets/star-outline.png"
     onClick={onClick}
+    onKeyDown={e => {
+      e.preventDefault()
+      if ((e.key === 'Enter' || e.key === ' ') && onClick) {
+        onClick()
+      }
+    }}
   />
 )
