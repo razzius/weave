63146084c05eb0c160c6080682734209c3351dd9
Razzi Abuissa <razzi53@gmail.com>
2020-04-07 09:15:19 -0700

    Update sorting test to properly specify date_updated

--- tests/test_get_profiles_sorting.py
+++ tests/test_get_profiles_sorting.py
@@ -6,22 +6,22 @@ from .utils import create_test_profile
 
 def test_sort_profiles_by_date_updated(client):
     own_profile = create_test_profile(
-        email='test@test.com',
-        name='Own Profile',
+        email="test@test.com",
+        name="Own Profile",
         date_updated=datetime.datetime(2018, 1, 1),
         available_for_mentoring=True,
     )
 
     recently_updated_profile = create_test_profile(
-        email='updated@test.com',
-        name='Z',
+        email="updated@test.com",
+        name="Z",
         date_updated=datetime.datetime(2019, 10, 1),
         available_for_mentoring=True,
     )
 
     not_recently_updated_profile = create_test_profile(
-        email='not_updated@test.com',
-        name='A',
+        email="not_updated@test.com",
+        name="A",
         date_updated=datetime.datetime(2017, 10, 1),
         available_for_mentoring=True,
     )
@@ -29,16 +29,16 @@ def test_sort_profiles_by_date_updated(client):
     own_token = own_profile.verification_email.verification_tokens[0].token
 
     response = client.get(
-        '/api/profiles?sort=date_updated',
-        headers={'Authorization': f'Token {own_token}'},
+        "/api/profiles?sorting=date_updated",
+        headers={"Authorization": f"Token {own_token}"},
     )
 
     assert response.status_code == http.HTTPStatus.OK.value
 
-    assert response.json['profile_count'] == 3
+    assert response.json["profile_count"] == 3
 
-    results = response.json['profiles']
+    results = response.json["profiles"]
 
-    assert results[0]['id'] == own_profile.id
-    assert results[1]['id'] == recently_updated_profile.id
-    assert results[2]['id'] == not_recently_updated_profile.id
+    assert results[0]["id"] == own_profile.id
+    assert results[1]["id"] == recently_updated_profile.id
+    assert results[2]["id"] == not_recently_updated_profile.id
