3e8e925c033bc6017d9f9140a9b92b394fc781ae
Razzi Abuissa <razzi53@gmail.com>
2020-04-15 18:41:34 -0700

    Use 2 svgs for profile star

--- src/ProfileStar.js
+++ src/ProfileStar.js
@@ -12,7 +12,7 @@ export default ({
     type="image"
     alt="Star"
     className={`profile-star ${active ? 'active' : ''}`}
-    src="/assets/star-outline.png"
+    src={active ? "/assets/star-active.svg" : '/assets/star-inactive.svg'}
     onClick={onClick}
   />
 )
