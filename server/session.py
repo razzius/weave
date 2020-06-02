495896d978a2a95a70467245cd22e9cf0bb27323
Razzi Abuissa <razzi53@gmail.com>
2020-06-01 23:02:00 -0700

    More logging for token expiration

--- server/session.py
+++ server/session.py
@@ -5,6 +5,11 @@ from flask import current_app
 
 
 def token_expired(verification_token):
+    if verification_token.expired:
+        current_app.logger.info("token %s expired", verification_token.token)
+
+        return True
+
     hours_until_expiry = (
         168 * 2
         if verification_token.is_personal_device
@@ -15,10 +20,13 @@ def token_expired(verification_token):
         hours=hours_until_expiry
     )
 
-    if verification_token.expired:
-        current_app.logger.info("token %s expired", verification_token.token)
-
-        return True
+    current_app.logger.info(
+        "Token %s date_created %s is_personal_device: %s set to expire %s",
+        verification_token.token,
+        verification_token.date_created,
+        verification_token.is_personal_device,
+        expire_time,
+    )
 
     current_time = datetime.datetime.utcnow()
 
