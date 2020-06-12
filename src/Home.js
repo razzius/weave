e2f1c2b17458623d663d6ff4b86b2d4faa0112a4
Razzi Abuissa <razzi53@gmail.com>
2020-06-11 22:11:44 -0500

    Add welcome message to home page and redirect to that upon successful login

--- src/Home.js
+++ src/Home.js
@@ -1,17 +1,24 @@
 // @flow
 import React from 'react'
-import { LiteralLink } from './utils'
 
+import { LiteralLink, getParam } from './utils'
 import { type Account } from './api'
 import HomeActions from './HomeActions'
 
 const Home = ({ account }: { account: ?Account }) => {
   const isMentor = Boolean(account && account.isMentor)
   const profileId = account && account.profileId
+  const welcomeParam = getParam('welcome')
 
   return (
     <div className="Home">
-      <p className="error banner" style={{ textAlign: 'center' }}>
+      {welcomeParam !== null && account != null && (
+        <p className="welcome banner">
+          Welcome back, {account.email}. You have successfully logged in.
+        </p>
+      )}
+
+      <p className="error banner">
         During the current COVID-19 pandemic, we are encouraging mentor and
         mentee pairs to meet over Zoom. All Harvard students and faculty receive
         free Zoom access available at:{' '}
