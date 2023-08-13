0ec7552bad32faa961e4dd5feba275bcf0dda4d0
Razzi Abuissa <razzi@abuissa.net>
2023-08-13 00:28:08 -0500

    Center background, remove covid banner

--- src/Home.js
+++ src/Home.js
@@ -25,16 +25,6 @@ const Home = ({ account }: { account: ?Account }) => {
 
   return (
     <div className="Home">
-      <p className="error banner">
-        During the current COVID-19 pandemic, we are encouraging mentor and
-        mentee pairs to meet over Zoom. All Harvard students and faculty receive
-        free Zoom access available at:{' '}
-        <LiteralLink href="https://harvard.zoom.us/" />. This recommendation
-        will remain active throughout the COVID-19 pandemic. More information
-        can be found on the CDC&apos;s website:
-        <br />
-        <LiteralLink href="https://www.cdc.gov/coronavirus/2019-ncov/index.html" />
-      </p>
       <div id="background">
         {welcomeMessage}
         <div className="App-intro">
