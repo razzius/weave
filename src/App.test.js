09cd085af4d7aa29ef7464f2171db6032b6ec3d7
Razzi Abuissa <razzi@abuissa.net>
2023-08-21 16:29:21 -0500

    Update src/App.test.js to latest react render

--- src/App.test.js
+++ src/App.test.js
@@ -1,11 +1,10 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { createRoot } from 'react-dom/client'
 import App from './App'
 
 const { test } = global
 
 test('renders without crashing', () => {
-  const div = document.createElement('div')
-  ReactDOM.render(<App />, div)
-  ReactDOM.unmountComponentAtNode(div)
+  const root = createRoot(document.createElement('div'))
+  root.render(<App />)
 })
